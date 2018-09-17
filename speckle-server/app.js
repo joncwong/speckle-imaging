34eafb4f65107e5357d10756e4e98756d0a40523
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-09-17 13:21:27 -0700

    Missing files from previous commit

--- speckle-server/app.js
+++ speckle-server/app.js
@@ -1,8 +1,32 @@
 var express = require('express')
 var app = express()
+require('dotenv').config();
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+
+
+// Retrieve all olist
+app.get('/olist', function (req, res) {
+  con.query('SELECT * FROM dec14raw', function (error, results, fields) {
+      if (error) throw error;
+      return res.send({ error: false, data: results, message: 'olist files.' });
+  });
+});
+
 app.listen(3000)
