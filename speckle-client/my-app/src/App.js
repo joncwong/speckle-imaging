02fc460f735504d9e206fbeab52d9895b23ebe8a
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-09-26 13:45:30 -0700

    Add search bar skeleton component

--- speckle-client/my-app/src/App.js
+++ speckle-client/my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
 class App extends Component {
@@ -8,12 +9,28 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Speckle Imaging</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TextField
+          id="filled-full-width"
+          label="Label"
+          style={{ margin: 8}}
+          placeholder="Placeholder"
+          helperText="Full width!"
+          fullWidth
+          margin="normal"
+          variant="filled"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        
       </div>
+      
     );
   }
 }
