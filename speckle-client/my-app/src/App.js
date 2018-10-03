b9e497934690290d3a8078865a49b9aa8da36c90
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-10-03 12:39:22 -0700

    Add placeholder searchbar

--- speckle-client/my-app/src/App.js
+++ speckle-client/my-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import TextField from '@material-ui/core/TextField';
 import './App.css';
 
@@ -7,20 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Speckle Imaging</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1> Speckle Imaging Database</h1>
         <TextField
           id="filled-full-width"
-          label="Label"
-          style={{ margin: 8}}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
+          label="Search for a Star ID"
+          style={{ width: 600}}
+          placeholder="e.g H602009"
           margin="normal"
           variant="filled"
           InputLabelProps={{
@@ -28,7 +19,6 @@ class App extends Component {
           }}
         />
 
-        
       </div>
       
     );
