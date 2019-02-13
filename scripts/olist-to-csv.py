d101f7757021358e662a884c21c71123b8fa40d5
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-02-13 14:23:03 -0800

    Bug fix on inconsistent columns

--- scripts/olist-to-csv.py
+++ scripts/olist-to-csv.py
@@ -8,11 +8,21 @@ import astropy.coordinates as coord
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-file_name = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+# Opens a dialog box for user to select .olist file to convert 
+Tk().withdraw() 
+file_name = askopenfilename() 
 print(file_name)
 
+
 def query_simbad(ra, dec):
+    """
+        Return a dictionary of Simbad results given a coordinate set.
+        Parameters: 
+        ra (string): Right Ascension 
+        dec (string): Declination 
+        Returns:
+        dict: Contains key/values with valuable SIMBAD query information
+    """
     simbad_results = {} 
     objects = {}
     customSimbad = Simbad()
@@ -50,10 +60,11 @@ def query_simbad(ra, dec):
     simbad_results['objects'] = objects
     return simbad_results
 
+
 with open(file_name) as file:
     data = file.read()
 
-new_data = []
+new_data = [] 
 head, tail = os.path.split(file_name)
 split_by_line = data.split('\n')
 for line in split_by_line:
@@ -76,12 +87,16 @@ for line in split_by_line:
             #Grab only the first ten entries, separated by spaces in olist files
             line_split = line.split(" ")
             first_nine_entries = line_split[:9]
-
+            
+            # Sometimes, olist entry will have some missing data, 
+            # append the list with empty strings so indexing doesn't become inconsistent
+            empty_columns = 9 - len(first_nine_entries)
+            for i in range(empty_columns):
+                first_nine_entries.append("")
+                
             #Grab RA and Dec to retrieve SIMBAD ID to insert into column 11
             ra = first_nine_entries[5] 
             dec = first_nine_entries[6]
-            print("5: " + first_nine_entries[5])
-            print("6: " + first_nine_entries[6])
 
             first_nine_entries = "|".join(first_nine_entries)
             #Replace spaces in the tenth column/entry with underscores
@@ -96,12 +111,17 @@ for line in split_by_line:
             if not comment:
                 #Once again, strip any double spaces from joining the other entries
                 line = re.sub("\s\s+", " ", line)
+                line = line + "|"
             else:
-                line = line + "," + comment
+                line = line + "|" + comment
                 line = re.sub("\s\s+", " ", line)
             new_data.append(line)
+            print(line)
 
 with open("newbatch" + tail + ".csv", 'w+') as new_file:
+    #write header
+    header = "star_id|fits_file|time|blue_gain|red_gain|right_asc|dec|epoch|mag|program_id|objects|search_radius|comment"
+    new_file.write(header + "\n")
     for line in new_data:
         new_file.write(line + "\n")
         print("hello")
