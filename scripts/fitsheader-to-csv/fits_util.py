ef5da2c2b8eb5b8d8c00e3831ca3d301515b772d
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-11-18 16:05:51 -0800

    Add fits util file

---
+++ scripts/fitsheader-to-csv/fits_util.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+
+
+def is_dir_or_fits_file(path):
+    '''
+        Returns whether or not a path is a fits file or a directory
+
+        Example 1:
+            is_dir_or_fits_file('/fitsfiles/') -- Returns: True
+
+        Example 2:
+            is_dir_or_fits_file('/fitsfiles/hello.fits') -- Returns: True
+
+        Example 3:
+            is_dir_or_fits_file('hello.txt') -- Returns: False
+    '''
+    if os.path.isdir(path):
+        return True
+    else:
+        name, ext = os.path.splitext(path)
+        if ext == "fits":
+            return True
+    return False
+
+
+def find_all_fits_files(path):
+    '''
+        Returns a list of all the files in a given path
+    '''
+    fits_file_list = []
+
+    for fits_file in pathlib.Path(path).glob('**/*.fits'):
+        fits_file_list.append(fits_file)
+
+    return fits_file_list
