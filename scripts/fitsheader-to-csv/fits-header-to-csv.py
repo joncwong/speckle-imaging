00bd337acade7ce4b197436d3d47805d485f4a2d
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-11-21 12:46:58 -0800

    Add filters to fits scripts

--- scripts/fitsheader-to-csv/fits-header-to-csv.py
+++ scripts/fitsheader-to-csv/fits-header-to-csv.py
@@ -1,27 +1,28 @@
 from astropy.io import fits
+import fits_util
 import sys
 
 
 class FitsReader:
 
-    def is_dir_or_fits_file(path_name):
-        pass
-    
-
-    def traverse_fits_dir(dir_name):
-        pass
-
-    def prompt():
-        file_name = prompt_file_name()
-        fits_file = fits.open(file_name)
-        print(fits_file)
-        print(file_name)
-        print(fits_file.info())
-
-        for i in fits_file:
-            print(i)
+    def __init__(self, fits_files, headers):
+        self.fits_files = fits_files
+        self.headers = headers
 
 
-if __name__ == "__main__":
-    fitsReader = FitsReader()
-    fitsReader.prompt()
+def get_args():
+    '''
+        Return list of passed in arguments. Expects fits files or directories containing fits files
+    '''
+    args = sys.argv[1:]
+    return args
+
+
+def main():
+    path_list = get_args()
+    fits_files = fits_util.aggregate_fits_files(path_list)
+    headers = ['UTC', 'EPOCH', 'RA', 'DEC', 'EMGAIN', 'FILTER', 'OBSID', 'COMMENT']
+
+if __name__ == "__":
+    # main()
+    get_args()
--- scripts/fitsheader-to-csv/fits_util.py
+++ scripts/fitsheader-to-csv/fits_util.py
@@ -2,36 +2,41 @@ import os
 import pathlib
 
 
-def is_dir_or_fits_file(path):
+def is_fits_file(path):
     '''
-        Returns whether or not a path is a fits file or a directory
+        Returns a file is a fits file.
 
         Example 1:
-            is_dir_or_fits_file('/fitsfiles/') -- Returns: True
+            is_fits_file('/fitsfiles/hello.fits') -- Returns: True
 
         Example 2:
-            is_dir_or_fits_file('/fitsfiles/hello.fits') -- Returns: True
-
-        Example 3:
-            is_dir_or_fits_file('hello.txt') -- Returns: False
+            is_fits_file('hello.txt') -- Returns: False
     '''
     if os.path.isdir(path):
-        return True
-    else:
-        name, ext = os.path.splitext(path)
+        _, ext = os.path.splitext(path)
         if ext == "fits":
             return True
     return False
 
 
-def find_all_fits_files(path):
+def is_fits_or_dir(path):
+    '''
+        Return whether or not a path is either a fits file or a directory
+    '''
+    return is_fits_file(path) or os.path.isdir(path)
+
+
+def aggregate_fits_files(path_list):
     '''
         Recursively searches and returns a list of all the files (POSIX format)
         in a given path.
     '''
-    fits_file_list = []
+    fits_files = []
 
-    for fits_file in pathlib.Path(path).glob('**/*.fits'):
-        fits_file_list.append(fits_file)
+    # Filter out any invalid or nonexistent path names
+    valid_paths = filter(is_fits_or_dir, path_list)
 
-    return fits_file_list
+    for fits_file in pathlib.Path(valid_paths).glob('**/*.fits'):
+        fits_files.append(fits_file)
+
+    return fits_files
