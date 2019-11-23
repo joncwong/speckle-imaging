d0be3b6e1ebc0ce75d9dd17a435989f244144cf9
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-11-22 16:15:06 -0800

    Rename scripts to tools

---
+++ tools/fits-header-to-csv.py
@@ -0,0 +1,19 @@
+from astropy.io import fits
+import find_all_fits_files
+import sys
+
+
+class FitsReader:
+
+    def __init__(self, fits_file_list):
+        self.fits_file_list = fits_file_list
+
+
+def main():
+    pass
+    # fits_file_list = find_all_fits_files()
+    # fitsReader = FitsReader()
+
+
+if __name__ == "__main__":
+    main()
